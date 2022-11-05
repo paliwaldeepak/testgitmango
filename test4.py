6d06d44765024df3bb0fceff4a5273543b788d42
paliwaldeepak <114053130+paliwaldeepak@users.noreply.github.com>
2022-11-05 10:53:53 +0530

    this is my first code push in git xas

--- test4.py
+++ test4.py
@@ -9,3 +9,13 @@ for i in cursor.fetchall():
 
 print(l)
 print(type(l[0]))
+
+mydb = conn.connect(host = "localhost", user = "root" , passwd = "Paliwal@286")
+cursor = mydb.cursor()
+cursor.execute("select employee_id, emp_mailid from sudhanshu.ineuron")
+l = []
+for i in cursor.fetchall():
+    l.append(i)
+
+print(l)
+print(type(l[0]))
